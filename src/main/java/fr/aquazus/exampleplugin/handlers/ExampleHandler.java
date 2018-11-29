f43e99f725fa0910ba1418d472383dddf8a8de51
Aquazus <me@aquazus.fr>
2018-11-29 14:47:33 +0100

    Updated to D1Proxy 1.10

--- src/main/java/fr/aquazus/exampleplugin/handlers/ExampleHandler.java
+++ src/main/java/fr/aquazus/exampleplugin/handlers/ExampleHandler.java
@@ -1,12 +1,13 @@
 package fr.aquazus.exampleplugin.handlers;
 
+import fr.aquazus.d1proxy.handlers.PacketDestination;
 import fr.aquazus.d1proxy.handlers.PacketHandler;
 import fr.aquazus.d1proxy.network.ProxyClient;
 
 public class ExampleHandler implements PacketHandler {
 
     @Override
-    public boolean shouldForward(ProxyClient proxyClient, String packet) {
+    public boolean shouldForward(ProxyClient proxyClient, String packet, PacketDestination destination) {
         if (packet.startsWith("Im0153;")) {
             proxyClient.sendMessage("[ExamplePlugin] Hello World!");
         }
